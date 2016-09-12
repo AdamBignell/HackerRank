d966fabd6fce85375191a5e18cd2580cd8fb38e0
Adam Bignell <adambignell@gmail.com>
2016-09-11 22:31:15 -0700

    Added incomplete pathcheck.cpp

---
+++ Mathematics/Fundamentals/pathcheck.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int t;
+    for (int i = 0; i < t; i++){
+        int a;
+        cin >> a;
+        int b;
+        cin >> b;
+        int x;
+        cin >> x;
+        int y;
+        cin >> y;
+    }
+    return 0;
+}
